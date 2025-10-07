785d94fd2fd922734568207623495d78ef0ed7cc
Vesperlin <shuhany86@gmail.com>
2025-10-07 10:08:01 +0800

    Update sb-config.js

--- sb-config.js
+++ sb-config.js
@@ -1,4 +1,5 @@
-// sb-config.js  —— 只放公开 anon key，绝对不要放 service_role key
-export const SUPABASE_URL = "https://rcytjdvqyqbvuyzfcinu.supabase.co";     // ← 复制你项目 URL
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjeXRqZHZxeXFidnV5emZjaW51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDc2MzUsImV4cCI6MjA3NTEyMzYzNX0.RyG8f5IL_Yt0UL_rsrP7UILncM9Ek4TMIYq1dr2Zb-U";                   // ← 复制 anon public key
-export const BUCKET = "photos";                                     // 你已在用的桶名
+// sb-config.js
+// 只放 anon public key，不要放 service_role！
+export const SUPABASE_URL = "https://rcytjdvqyqbvuyzfcinu.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjeXRqZHZxeXFidnV5emZjaW51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDc2MzUsImV4cCI6MjA3NTEyMzYzNX0.RyG8f5IL_Yt0UL_rsrP7UILncM9Ek4TMIYq1dr2Zb-U";
+export const BUCKET = "photos"; // 你项目里一直在用的桶

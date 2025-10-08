a26aa32ca1ca182830a64c0d5363ffd8ca3b2bc4
Vesperlin <shuhany86@gmail.com>
2025-10-08 12:32:23 +0800

    Update sb-config.js

--- sb-config.js
+++ sb-config.js
@@ -1,7 +1,4 @@
-// sb-config.js
+// 公网只用 anon key；bucket 固定 photos
 export const SUPABASE_URL = "https://rcytjdvqyqbvuyzfcinu.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjeXRqZHZxeXFidnV5emZjaW51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDc2MzUsImV4cCI6MjA3NTEyMzYzNX0.RyG8f5IL_Yt0UL_rsrP7UILncM9Ek4TMIYq1dr2Zb-U";
 export const BUCKET = "photos";
-
-// 固定房间
-export const ROOM_ID = "1010";

bfe8ef92d51e17920445cb151e1aa159c27e1560
Vesperlin <shuhany86@gmail.com>
2025-10-08 11:39:24 +0800

    Update sb-config.js

--- sb-config.js
+++ sb-config.js
@@ -1,5 +1,7 @@
 // sb-config.js
-// 只放 anon public key，不要放 service_role！
 export const SUPABASE_URL = "https://rcytjdvqyqbvuyzfcinu.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjeXRqZHZxeXFidnV5emZjaW51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDc2MzUsImV4cCI6MjA3NTEyMzYzNX0.RyG8f5IL_Yt0UL_rsrP7UILncM9Ek4TMIYq1dr2Zb-U";
-export const BUCKET = "photos"; // 你项目里一直在用的桶
+export const BUCKET = "photos";
+
+// 固定房间
+export const ROOM_ID = "1010";
